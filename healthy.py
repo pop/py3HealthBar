171ccf6476defa05fcec36049545605cfac15b09
Elijah <homeworkbad@gmail.com>
2014-03-18 18:16:53 -0700

    v0.1 displays new # ever 5min

--- healthy.py
+++ healthy.py
@@ -1,8 +1,7 @@
 
-## Currently this script only displays the number of seconds that have passed since you started the script.
+#v 0.1 Currently displays an additional '#' for every five minutes i3status has been running.
 
 from time import time
-import os
 
 
 TIME = time()   #Gets time at startup
@@ -10,11 +9,21 @@ TIMEOUT = 1     #Sets cache dump timeout to 1 second
 
 # Begin Py3status stuffs
 class Py3status:
+
     def test(self, i3status_output_json, i3status_config):
         l_time = time()     #Gets new time
         delta = l_time - TIME   # Calculates delta since startup time
         delta = int(delta)  #Sets delta time to int
-        status = '{}'.format(delta) #formats output for py3status
+        
+        var = "" #Var is waht's getting passed to i3status eventually
+
+        x = delta/600 #x is the number of 5 minute increments that have passed since the bar began.
+        for i in range (0,x): #This increments through the x times and adds a # for every 5minute increment that has passed
+            var += str("#") #Actual assignment of var
+            
+        status = '{}'.format(var) #formats output for py3status
+
         response = {'name': 'healthbar', 'full_text': status}   #This line is magic
         response['cached_until'] = TIMEOUT  # sets cache timeout to TIMEOUT 
+
         return (0, response)    # returns values to be printed on the bar
